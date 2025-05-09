96d63592b1ba5b448cdbc34b306f6f2aca7872f2
djordjepvl <djpavlovic2123rn@raf.rs>
2025-05-09 14:09:27 +0200

    Project base

---
+++ Server/Start server.bat
@@ -0,0 +1,9 @@
+@echo off
+:: Check for admin rights
+net session >nul 2>&1
+if %errorLevel% NEQ 0 (
+    echo Requesting administrator privileges...
+    powershell -Command "Start-Process '%~f0' -Verb RunAs"
+    exit /b
+)
+net start MySQL80
